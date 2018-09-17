eb60f73ec16c5a48e1aabc809a17dbb208ac83e6
o m <sandrm@ukr.net>
2018-09-17 20:58:33 +0300

    Changes #2

--- src/main/java/SomeApp.java
+++ src/main/java/SomeApp.java
@@ -8,6 +8,8 @@ public class SomeApp {
 		SomeBean bean = new SomeBean();
 		
 		System.out.println("This is branch Master: changes #1");
+		
+		System.out.println("This is branch Master: changes #2");
 	}
 
 }
--- src/main/java/SomeBean.java
+++ src/main/java/SomeBean.java
@@ -2,5 +2,7 @@
 public class SomeBean {
 	public SomeBean() {
 		System.out.println("SomeBean created.");
+		
+		System.out.println("This is branch Master: changes #2");
 	}
 }
